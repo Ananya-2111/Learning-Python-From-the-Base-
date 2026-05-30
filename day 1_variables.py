4648d16d519e0e91150f3f08d7b7a17ae9abc942
ANANYA <audacious.ananya@gmail.com>
2026-05-30 11:57:59 +0530

    Add day 1_variables.py

---
+++ day 1_variables.py
@@ -0,0 +1,8 @@
+name = 'Ananya'
+Name = "Ananya"
+NAME = 'Ananya'
+Age = 19
+City = 'Delhi'
+print(" all these " , name,Name, NAME,"have different locations . they are not the same ")
+print("My name is ",name,". I am ",Age,"years old . And I live in", City)
+print("This is what i learn't about variables. ")
