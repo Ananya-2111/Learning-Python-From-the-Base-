9a7732819fd447ebfb3316c8878d2b70e5769bf2
ANANYA <audacious.ananya@gmail.com>
2026-05-30 17:06:44 +0530

    Add day 5_chained_conditionals_nested.py

---
+++ day 5_chained_conditionals_nested.py
@@ -0,0 +1,16 @@
+x=2
+y=3
+if (y == x or x + y == 5):
+    print('ran')
+else:
+    print('ok')
+
+a=3
+b=4
+if a == 3:
+    if b == 4:
+        print('a = 3,y=4')
+    else:
+        print('a =3, b!= 4')
+else:
+    print('a != 3')
